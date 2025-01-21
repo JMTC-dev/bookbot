23e8a587d1d058c7b864c1ac5cf48677eb69f6c8
JMTC-dev <jackmatthewcasey@gmail.com>
2025-01-21 20:45:49 +1000

    count alphanumeric characters and generate a report of words and character totals

--- main.py
+++ main.py
@@ -2,9 +2,9 @@ def main():
     book_path = "books/frankenstein.txt"
     text = get_book_text(book_path)
     num_words = count_number_of_words(text)
-    num_characters = count_number_of_letters(text)
-    print(f"{num_words} words found in the document")
-    print(num_characters)
+    num_characters = count_number_of_letters(text, True)
+    generate_report(num_words, num_characters, book_path)   
+
 
 def count_number_of_words(content):
     words = content.split()
@@ -14,15 +14,31 @@ def get_book_text(path):
     with open(path) as f:
         return f.read()
 
-def count_number_of_letters(content):
+def count_number_of_letters(content, alpha):
     character_dict = {}
     for c in content:
         char = c.lower()
-        if(char in character_dict):
-            character_dict[char] += 1
+        if(not alpha):
+            if(char in character_dict):
+                character_dict[char] += 1
+            else:
+                character_dict[char] = 1
         else:
-            character_dict[char] = 1
+            if(char.isalpha()):
+                if(char in character_dict):
+                    character_dict[char] += 1
+                else:
+                    character_dict[char] = 1
+        
     return character_dict
+
+def generate_report(num_words, num_characters, book_path):
+    print(f"--- Begin report of {book_path} --- ")
+    print(f"{num_words} words found in the document \n")
+    for char in num_characters:
+        print(f"The '{char}' character was found {num_characters[char]} times")
+    print("--- End report ---")
+
 main()
 
 
